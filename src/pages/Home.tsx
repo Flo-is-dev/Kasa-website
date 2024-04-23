27adbb4f1c5c9221eb1613a59ca3f64ebe5fa36f
Flo-le-dev <f.gironde.dev@gmail.com>
2024-04-24 00:01:33 +0200

    ended p11 Typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
-
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
@@ -9,7 +8,28 @@ import {DataAPI} from "../model";
 
 
 const Home = () => {
-  let data:DataAPI[]= require("../data/data.json");
+//   let data:DataAPI[]= require("../data/data.json");
+//   console.log(data);
+
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("data/data.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   console.log(data);
 
   return (
@@ -19,7 +39,7 @@ const Home = () => {
         <Header children={<h1>Chez vous, partout et ailleurs</h1>} />
         <div className="logementCardContainer">
           <ul>
-            {data.map((logement:DataAPI) => {
+            { data.map((logement:DataAPI) => {
               return <Card key={logement.id} logement={logement} />;
             })}
           </ul>
